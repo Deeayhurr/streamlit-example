df234a403619cd925c4bc5decacbec65eb73a70b
Adeyemi Adedayo <35253736+Deeayhurr@users.noreply.github.com>
2022-08-01 11:20:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,17 @@ df = pd.DataFrame({
 })
 
 df
+
+
+import streamlit as st
+import numpy as np
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
